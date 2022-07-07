93ab5c6737798b16f6d39fed738e90c9d4b1e208
itedya <kontakt@itedya.com>
2022-07-07 09:59:08 +0200

    Added arudino sessions service to arduino configurations controller

--- controllers/arduino-configurations.js
+++ controllers/arduino-configurations.js
@@ -1,4 +1,5 @@
 import * as arduinoConfigurationsService from "../services/arduino-configurations.js";
+import * as arduinoServicesService from "../services/arduino-sessions.js";
 import ArduinoConfigurationDto from "../dtos/arduino-configuration.js";
 
 const getArduinoConfigurations = async (req, res) => {
@@ -13,12 +14,16 @@ const createArduinoConfiguration = async (req, res) => {
 
     const result = await arduinoConfigurationsService.createConfiguration(dto);
 
+    arduinoServicesService.createArduinoSession(result);
+
     res.json(result)
         .status(201);
 }
 
 const deleteArduinoConfiguration = async (req, res) => {
     const {uuid} = req.body;
+
+    await arduinoServicesService.closeArduinoSession(uuid);
 
     await arduinoConfigurationsService.deleteArduinoConfigurationByUuid(uuid);
 
@@ -29,6 +34,9 @@ const updateArduinoConfiguration = async (req, res) => {
     const dto = new ArduinoConfigurationDto(req.body);
 
     const result = await arduinoConfigurationsService.updateArduinoConfiguration(dto);
+
+    await arduinoServicesService.closeArduinoSession(result);
+    arduinoServicesService.createArduinoSession(result);
 
     res.json(result)
         .status(200);
